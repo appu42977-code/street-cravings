fbc9e3620b9f938b5e751bede859893365933576
25bcnb51-dotcom <25bcnb51@kristujayanti.com>
2026-03-30 20:27:06 +0530

    update db config

---
+++ backend/db.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: 4000,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: false
+  }
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log("❌ DB connection failed:", err);
+  } else {
+    console.log("✅ TiDB Connected");
+  }
+});
+
+module.exports = db;
